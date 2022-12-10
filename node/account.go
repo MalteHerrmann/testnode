361fe901c54f9f1b1ec5c45737a1763a6b9cdb62
MalteHerrmann <malte@evmos.org>
2022-12-10 02:52:27 +0100

    address last linter comment

--- node/account.go
+++ node/account.go
@@ -3,12 +3,12 @@ package node
 // Account defines the implementation of an Evmos account
 // in the context of the testnode tool.
 type Account struct {
-	//// balance is the account's balance
-	//balance string
-	//// bech32Address is the account address in bech32 format
-	//bech32Address string
-	//// hexAddress is the account address in hex format
-	//hexAddress string
+	// // balance is the account's balance
+	// balance string
+	// // bech32Address is the account address in bech32 format
+	// bech32Address string
+	// // hexAddress is the account address in hex format
+	// hexAddress string
 	// name is the key name
 	name string
 }
